1aa6535689452df362ccb41c02bee5dcb73f1290
Naeltonf <naeltonfdasilva@gmail.com>
2024-07-10 10:35:49 -0300

    creat component creation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import './App.css';
-import logo from './imagens/logo.svg'
+import "./App.css";
+import Logo from "./componentes/Logo";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <div className='logo'>
-        <img src={logo}></img>
-        </div>
-        
+    <div className="App">
+      <header className="App-header">
+       <Logo></Logo>  
       </header>
     </div>
   );
 }
 
-export default App;
-
-
-teste
+export default App
