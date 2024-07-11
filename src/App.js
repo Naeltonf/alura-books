48947aae0b29a122ab84d5d048e0149db73f6ff3
Naeltonf <naeltonfdasilva@gmail.com>
2024-07-11 11:08:02 -0300

    updated header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import Logo from "./componentes/Logo";
+import OpcoesHeader from "./componentes/OpcoesHeader";
 import perfil from "./imagens/perfil.svg";
 import sacola from "./imagens/sacola.svg";
 
-const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"];
 const icones = [perfil, sacola];
 
 function App() {
@@ -11,13 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Logo></Logo>
-        <ul className="opcoes">
-          {textoOpcoes.map((texto) => (
-            <li className="opcao">
-              <p>{texto}</p>
-            </li>
-          ))}
-        </ul>
+        <OpcoesHeader></OpcoesHeader>
 
         <ul className="icones">
           {icones.map((icone) => (
