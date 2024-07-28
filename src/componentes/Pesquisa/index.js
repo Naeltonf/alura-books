1f3a8ffbf898e18dd0dce76abf54c96f3c0fe4be
Naeltonf <naeltonfdasilva@gmail.com>
2024-07-28 17:50:22 -0300

    updated component state

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,5 +1,6 @@
 import Input from "../Input"
 import styled from "styled-components"
+import { useState } from "react"
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%. #326589 165%);
@@ -12,7 +13,7 @@ const PesquisaContainer = styled.section`
 
 const Titulo = styled.h2`
     color: #FFF;
-    font-size: 6px;
+    font-size: 36px;
     text-align: center;
     width: 100%;
 `
@@ -24,13 +25,17 @@ const Subtitulo = styled.h3`
 `
 
 function Pesquisa() {
+    const [textoDigitado, setTextoDigitado] = useState('')
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nosa estante</Subtitulo>
             <Input
                 placeholder="Escreva sua próxima leitura"
+                onBlur={evento => setTextoDigitado(evento.target.value)}
             />
+            <p>{textoDigitado}</p>
         </PesquisaContainer>
     )
 }
